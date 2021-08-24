5f144c95845ec806c123fd2d9aeb6e04b3fe75ea
Ben Gross <benjamingro@gmail.com>
2021-08-24 15:55:13 -0500

    fixed hamburger issue

--- src/app/my-navbar/my-navbar.component.ts
+++ src/app/my-navbar/my-navbar.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-my-navbar',
   templateUrl: './my-navbar.component.html',
-  styleUrls: ['./my-navbar.component.scss']
+  styleUrls: ['./my-navbar.component.scss'],
 })
 export class MyNavbarComponent implements OnInit {
+  public current_route: string = '/about';
 
-  public current_route : string = '/about'; 
+  public navbar_menu_expand_mobile: boolean = false;
 
-  constructor(private location : Location) { 
-    location.onUrlChange((url: string, state: unknown):void=>{
-      this.current_route = url; 
+  constructor(private location: Location) {
+    location.onUrlChange((url: string, state: unknown): void => {
+      this.current_route = url;
     });
   }
 
-  ngOnInit(): void {
-  }
+  toggleNavbarMenu() : void {
+    this.navbar_menu_expand_mobile = !this.navbar_menu_expand_mobile; 
+   }
 
+  // expandNavbarMenu(): void {
+  //   this.navbar_menu_expand_mobile = true;
+  // }
+
+  // collapseNavebarMenu() : void { 
+  //   this.navbar_menu_expand_mobile = false; 
+  // }
+
+  ngOnInit(): void {}
 }
